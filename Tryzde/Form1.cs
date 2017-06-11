3e6a35690e7eaa89b7cabc4a907bc67058e8cb83
Mindaugas Varkalys <varkalys.mindaugas@gmail.com>
2017-06-11 22:03:16 +0300

    Things done in Telia IoT hackathon

---
+++ Tryzde/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WebSocketSharp;
+
+namespace Tryzde
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            using (StreamReader reader = new StreamReader(openFileDialog1.OpenFile()))
+            {
+                using (var ws = new WebSocket("ws://10.20.0.30"))
+                {
+                    string line;
+                    ws.Connect();
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        ws.Send(line);
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+    }
+}
